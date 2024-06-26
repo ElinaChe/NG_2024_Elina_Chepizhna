72424507c1dcaa0b8e94cb53996ae4f966404812
ElinaChe <156594109+ElinaChe@users.noreply.github.com>
2024-06-26 13:46:39 +0200

    lesson 1

---
+++ Lesson 1/task2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double x, y;
+
+    cout << "vvedite chisla ";
+    cin >> x >> y;
+
+    switch (operation) {
+        cout << "rezultat: " << x + y << endl;
+
+    }
+
+}
